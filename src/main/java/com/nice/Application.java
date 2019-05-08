5e3699d45628b676b76ea4dd18e8e3268f0aa1cd
nice <996099092@qq.com>
2019-05-08 15:50:07 +0800

    首次上传项目

---
+++ src/main/java/com/nice/Application.java
@@ -0,0 +1,13 @@
+package com.nice;
+
+import net.unicon.cas.client.configuration.EnableCasClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableCasClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
